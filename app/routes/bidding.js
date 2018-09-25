5cbcb85fc891912a1598c5a62245844bf7a8fea1
Tarun B <tarun.bhuthapuri@zoomrx.com>
2018-09-25 14:42:17 +0530

    Initial response successful

--- app/routes/bidding.js
+++ app/routes/bidding.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	model() {
+		return Ember.$.ajax({
+			url: 'http://localhost/CarromTournamentServer/users.json',
+			method: "GET",
+			dataType: 'json',
+		}).then(players => {
+			return players;
+		}, (err) => {
+			console.log(err);
+		});
+	}
 });
